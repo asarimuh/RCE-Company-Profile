d8d10378266ab9781ff8e16352c7245601a6e9d7
Asari <asarimuh12@gmail.com>
2026-05-18 14:00:44 +0700

    feat: Created Digital Entertainment page, Corporate & Legal Solutions page, E-Commerce page

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,24 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  root: 'src', // Set the source folder as root
+  root: 'src', 
   build: {
-    outDir: '../dist', // Output the build to the root's dist folder
+    outDir: '../dist', 
     emptyOutDir: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
         blog: resolve(__dirname, 'src/blog/index.html'),
+        digitalEntertainment: resolve(__dirname, 'src/digital-entertainment.html'),
+        corporateSolutions: resolve(__dirname, 'src/corporate-solutions.html'),
+        eCommerce: resolve(__dirname, 'src/e-commerce.html'),
+
         // Add more pages here as you create them
       },
     },
   },
   server: {
     port: 3000,
-    open: true, // Automatically opens the browser
+    open: true, 
   },
 });
